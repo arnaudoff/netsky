877eda54aafedf8986e15b9f245c787622bb77eb
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2016-09-03 17:04:36 +0300

    Add an abstract Server and WebSocketServer implementation

---
+++ sniffer/src/core/communications/include/Server.hpp
@@ -0,0 +1,35 @@
+#ifndef SERVER_HPP_
+#define SERVER_HPP_
+
+#include <sstream>
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "../../../core/include/PolicyBindings.hpp"
+
+namespace Sniffer {
+    namespace Core {
+        namespace Communications {
+            class Server {
+                private:
+                    std::shared_ptr<ConfigManager> config_manager_;
+
+                public:
+                    Server(std::shared_ptr<ConfigManager> manager)
+                        : config_manager_{manager}
+                    {};
+
+                    virtual void start(uint16_t port) = 0;
+
+                    virtual void stop() = 0;
+
+                    virtual void broadcast(const std::string& message) = 0;
+
+                    virtual ~Server();
+            };
+        }
+    }
+}
+
+#endif
