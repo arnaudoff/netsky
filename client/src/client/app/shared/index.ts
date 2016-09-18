a8cfe1d45b77fb619cced3f598a97b3c02db6c34
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2016-09-18 21:21:44 +0300

    Add shared module with a navbar shared component

---
+++ client/src/client/app/shared/index.ts
@@ -0,0 +1,7 @@
+/**
+ * This barrel file provides the exports for the shared resources (services, components).
+ */
+export * from './websocket/index';
+export * from './sniffer-client/index';
+export * from './navbar/index';
+export * from './config/env.config';
---
+++ client/src/client/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { NavbarComponent } from './navbar/index';
+import { WebSocketService } from './websocket/index';
+import { SnifferClientService } from './sniffer-client/index';
+
+/**
+ * Do not specify providers for modules that might be imported by a lazy loaded module.
+ */
+
+@NgModule({
+  imports: [CommonModule, RouterModule],
+  declarations: [NavbarComponent],
+  exports: [NavbarComponent, CommonModule, FormsModule, RouterModule]
+})
+
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [WebSocketService, SnifferClientService]
+    };
+  }
+}
