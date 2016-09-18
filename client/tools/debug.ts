c1f1962fbea4757cfdf5100eb98154ecec667238
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2016-09-18 21:17:26 +0300

    Add client configurations, gulp tasks and other boilerplate code

---
+++ client/tools/debug.ts
@@ -0,0 +1,16 @@
+import * as gulp from 'gulp';
+import { argv } from 'yargs';
+
+require('../gulpfile');
+
+const TASK = argv['task'];
+
+if (!TASK) {
+  throw new Error('You must specify a task name.');
+}
+
+console.log('**********************');
+console.log('* debugging task:', TASK);
+console.log('**********************');
+
+gulp.start(TASK);
