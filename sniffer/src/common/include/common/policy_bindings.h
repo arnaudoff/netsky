516759a08c0dcd37c994c0cbbb3e48ab33c057bc
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2017-01-04 09:47:51 +0200

    Add the improved policy_bindings and test executable skeletons

--- sniffer/src/common/include/common/policy_bindings.h
+++ sniffer/src/common/include/common/policy_bindings.h
@@ -1,23 +1,50 @@
-#ifndef POLICY_BINDINGS_HPP_
-#define POLICY_BINDINGS_HPP_
+/*
+ * Copyright (C) 2016  Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
+ * Author: Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include "../../utils/include/ConfigurationManager.hpp"
-#include "../../utils/include/FileStoragePolicy.hpp"
-#include "../../utils/include/JsonFormattingPolicy.hpp"
+#ifndef SNIFFER_SRC_COMMON_INCLUDE_COMMON_POLICY_BINDINGS_H_
+#define SNIFFER_SRC_COMMON_INCLUDE_COMMON_POLICY_BINDINGS_H_
 
-#include "../../communications/serialization/include/SerializationManager.hpp"
-#include "../../communications/serialization/include/JsonSerializationPolicy.hpp"
+#include "common/config/configuration_manager.h"
+#include "common/config/file_storage_policy.h"
+#include "common/config/json_formatting_policy.h"
+
+#include "common/serialization/json_serialization_policy.h"
+#include "common/serialization/serialization_manager.h"
 
 namespace sniffer {
 
-}
+namespace common {
 
-typedef Sniffer::Utils::ConfigurationManager<
-    Sniffer::Utils::FileStoragePolicy,
-    Sniffer::Utils::JsonFormattingPolicy> ConfigurationMgr;
+namespace config {
 
-typedef Sniffer::Communications::Serialization::SerializationManager<
-    Sniffer::Communications::Serialization::JsonSerializationPolicy>
-    SerializationMgr;
+typedef ConfigurationManager<FileStoragePolicy, JsonFormattingPolicy>
+    ConfigurationMgr;
 
-#endif
+}  // namespace config
+
+namespace serialization {
+
+typedef SerializationManager<JsonSerializationPolicy> SerializationMgr;
+
+}  // namespace serialization
+
+}  // namespace common
+
+}  // namespace sniffer
+
+#endif  // SNIFFER_SRC_COMMON_INCLUDE_COMMON_POLICY_BINDINGS_H_
